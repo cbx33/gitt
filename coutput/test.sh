68e82c5ade949f094e9b42428ea0d6833e71c292
Peter Savage <silentkeystroke@googlemail.com>
2011-07-16 17:12:30 +0100

    Updated Script (NOT WORKING)

--- coutput/test.sh
+++ coutput/test.sh
@@ -1,5 +1,17 @@
 BASE="/home/john/testscript"
 OUTPUT="/home/john/outputs"
+START=0
+
+function runcommand {
+	if [ "$2" == "" ]; then
+		echo "$1" > ${OUTPUT}/`printf "%02d" $START`.txt
+		$1 &>> ${OUTPUT}/`printf "%02d" $START`.txt
+	else
+		echo "$2" > ${OUTPUT}/`printf "%02d" $START`.txt
+		$1 &>> ${OUTPUT}/`printf "%02d" $START`.txt
+	fi
+	let "START += 1"
+}
 
 if [ ! -d "$BASE" ]; then
 	mkdir $BASE
@@ -9,32 +21,36 @@ if [ ! -d "$OUTPUT" ]; then
 	mkdir $OUTPUT
 fi
 
-# Change Directory to the BASE
-cd $BASE
-
+# Change Directory to the OUTPUT and cleanup
+cd $OUTPUT
 rm * -Rf
 
-mkdir coderepo
-cd coderepo
-git init &> "${OUTPUT}/1.txt"
-touch my_first_committed_file &> "${OUTPUT}/2.txt"
-git add my_first_committed_file &> "${OUTPUT}/3.txt"
-git commit -m "My First Ever Commit" &> "${OUTPUT}/4.txt"
-echo "Change1" > my_first_committed_file &> "${OUTPUT}/5.txt"
-touch my_second_committed_file &> "${OUTPUT}/6.txt"
-touch my_third_committed_file &> "${OUTPUT}/7.txt"
-git --no-pager status &> "${OUTPUT}/8.txt"
-git add my_first_committed_file &> "${OUTPUT}/9.txt"
-echo "Change1" > my_second_committed_file &> "${OUTPUT}/10.txt"
-git add my_second_committed_file &> "${OUTPUT}/11.txt"
-git --no-pager status &> "${OUTPUT}/12.txt"
-echo "Change2" >> my_second_committed_file &> "${OUTPUT}/13.txt"
-git --no-pager status &> "${OUTPUT}/14.txt"
-git commit -m "Made a few changes to first and second files" &> "${OUTPUT}/15.txt"
-git status &> "${OUTPUT}/16.txt"
-git commit -a -m "Finished adding initial files" &> "${OUTPUT}/17.txt"
-git status &> "${OUTPUT}/18.txt"
-git add my_third_committed_file &> "${OUTPUT}/19.txt"
-git status &> "${OUTPUT}/20.txt"
-git reset my_third_committed_file &> "${OUTPUT}/21.txt"
-git status &> "${OUTPUT}/22.txt"
+# Change Directory to the BASE and cleanup
+cd $BASE
+rm * -Rf
+
+runcommand 'mkdir coderepo' 'mkdir coderepo'
+runcommand 'cd coderepo'
+runcommand 'git init'
+runcommand 'touch my_first_committed_file'
+runcommand 'git add my_first_committed_file'
+runcommand 'git commit -m "My First Ever Commit"'
+runcommand 'echo "Change1" > my_first_committed_file'
+runcommand 'touch my_second_committed_file'
+runcommand 'touch my_third_committed_file'
+runcommand 'git --no-pager status' 'git status'
+runcommand 'git add my_first_committed_file'
+runcommand 'echo "Change1" > my_second_committed_file'
+runcommand 'git add my_second_committed_file'
+runcommand 'git --no-pager status' 'git status'
+runcommand 'echo "Change2" >> my_second_committed_file'
+runcommand 'git --no-pager status' 'git status'
+runcommand 'git commit -m "Made a few changes to first and second files"'
+runcommand 'git --no-pager status' 'git status'
+runcommand 'git commit -a -m "Finished adding initial files"'
+runcommand 'git --no-pager status' 'git status'
+runcommand 'git add my_third_committed_file'
+runcommand 'git --no-pager status' 'git status'
+runcommand 'git reset my_third_committed_file'
+runcommand 'git --no-pager status' 'git status'
+
